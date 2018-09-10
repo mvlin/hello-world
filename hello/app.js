206c7f71cc957175eb485408513e7bd6b441541c
mvlin <40884311+mvlin@users.noreply.github.com>
2018-09-10 17:15:35 +0800

    Update app.js
    的的

--- hello/app.js
+++ hello/app.js
@@ -13,7 +13,7 @@ app.use(async (ctx, next) => {
 app.use(async (ctx, next) => {
     await next();
     ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, 林木为!</h1>';
+    ctx.response.body = '<h1>Hello, 林!</h1>';
 });
 
 app.listen(3000);
