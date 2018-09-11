feb3f1d44f48cc5b4692e07355ff43d7ec7c9793
mvlin <40884311+mvlin@users.noreply.github.com>
2018-09-11 17:35:26 +0800

    Update app.js

--- hello/app.js
+++ hello/app.js
@@ -13,7 +13,7 @@ app.use(async (ctx, next) => {
 app.use(async (ctx, next) => {
     await next();
     ctx.response.type = 'text/html';
-    ctx.response.body = '<h1>Hello, 林mnegewimnegwei!</h1>';
+    ctx.response.body = '<h1>Hello, 林梦威!</h1>';
 });
 
 app.listen(3000);
